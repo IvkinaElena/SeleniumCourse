37de65909fb5d1ef2d077878cf4c6cea50156fdd
IvkinaElena <sergeeva08em1@gmail.com>
2021-03-30 14:23:30 +0300

    third task

--- src/test/java/LoginAdminTest.java
+++ src/test/java/LoginAdminTest.java
@@ -1,13 +1,15 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class OpenURLTest {
+public class LoginAdminTest {
     private WebDriver driver;
 
     @BeforeAll
@@ -17,9 +19,12 @@ public class OpenURLTest {
     }
 
     @Test
-    public void testOpenURL() {
-        String url = "https://google.com";
+    public void testLoginAdmin() {
+        String url = "http://localhost/litecart/admin/";
         driver.get(url);
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
     }
 
     @AfterAll

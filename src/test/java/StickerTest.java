ebd8a77ae93d9b0b8552af2832ee4b646cc1e885
IvkinaElena <sergeeva08em1@gmail.com>
2021-04-07 23:51:36 +0300

    eighth task

---
+++ src/test/java/StickerTest.java
@@ -0,0 +1,44 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class StickerTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testSticker() {
+        driver.get("http://localhost/litecart");
+        for(WebElement element : driver.findElements(By.cssSelector("#box-most-popular li"))) {
+            assertTrue(element.findElement(By.cssSelector(".sticker")).isDisplayed());
+        }
+        for(WebElement element : driver.findElements(By.cssSelector("#box-campaigns li"))) {
+            assertTrue(element.findElement(By.cssSelector(".sticker")).isDisplayed());
+        }
+        for(WebElement element : driver.findElements(By.cssSelector("#box-latest-products li"))) {
+            assertTrue(element.findElement(By.cssSelector(".sticker")).isDisplayed());
+        }
+    }
+
+    @AfterAll
+    public void tearDown() {
+        driver.quit();
+    }
+}
